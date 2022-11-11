9446750e94ec2421ad6b8a40a50ca7f61d057e48
Kalka <k@kalka.io>
2022-11-11 05:17:30 -0500

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ["avatars.githubusercontent.com", "db17gxef1g90a.cloudfront.net"],
   }
 }
