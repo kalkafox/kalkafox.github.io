e542bf233b1890dff42ca8300d2b33bf08e14b1f
Kalka <k@kalka.io>
2023-01-19 16:28:10 -0500

    process version schema before building

--- deno/process_version.ts
+++ deno/process_version.ts
@@ -1,11 +1,17 @@
 import packageData from '../package.json' assert { type: 'json' }
+import * as log from "https://deno.land/std@0.171.0/log/mod.ts"
 
 const version_data = {
   ASTRO_VERSION: packageData.dependencies.astro.replace('^', ''),
   PROJECT_VERSION: packageData.version,
 }
 
+log.info(`ASTRO_VERSION = ${version_data.ASTRO_VERSION}`)
+log.info(`PROJECT_VERSION = ${version_data.PROJECT_VERSION}`)
+
 await Deno.writeTextFile(
   './src/util/version.ts',
   `export default ${JSON.stringify(version_data)}`,
 )
+
+log.info('Version data written to src/util/version.ts')
--- src/util/version.ts
+++ src/util/version.ts
@@ -1,1 +1,1 @@
-export default {"ASTRO_VERSION":"1.7.0","PROJECT_VERSION":"0.0.1"}
+export default {"ASTRO_VERSION":"1.7.0","PROJECT_VERSION":"0.5.0"}
